187cd92499cb1d7e1e2ca3bd91f178b306513191
lindx <linder.igor@gmail.com>
2021-01-14 04:39:02 +0300

    add hibernate property

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,20 +5,50 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.properties.HibernateProperty;
 import jm.task.core.jdbc.util.properties.JDBCProperty;
 import jm.task.core.jdbc.util.properties.LoggerProperty;
 
 public class Util {
 
-    public static Connection getConnection() throws SQLException {
+    private static SessionFactory sessionFactory = null;
+    private static Configuration configuration = null;
 
+    static{
         JDBCProperty.getMySqlDriver();
+        LoggerProperty.readConfiguration();
+        configuration = HibernateProperty.getConfiguration();
+
+        configuration.addAnnotatedClass(User.class);
+        sessionFactory = buildSessionFactory();
+    }
+
+    public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBCProperty.getURL(), JDBCProperty.getUser(), JDBCProperty.getPass());
     }
 
     public static Logger getLogger(){
-    
-        LoggerProperty.readConfiguration();
         return Logger.getLogger(Util.class.getSuperclass().getName());
+    }
+
+    private static SessionFactory buildSessionFactory() {
+
+        try {
+            return configuration.buildSessionFactory();
+        }catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/properties/HibernateProperty.java
+++ src/main/java/jm/task/core/jdbc/util/properties/HibernateProperty.java
@@ -1,5 +1,32 @@
 package jm.task.core.jdbc.util.properties;
 
+import java.util.Properties;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 public class HibernateProperty {
     
+    private static Properties properties = null;
+    private static Configuration cfg = null;
+
+    static {
+        properties = new Properties();
+        properties.setProperty(Environment.DRIVER, JDBCProperty.getMySqlDriver().getName());
+        properties.setProperty(Environment.URL, JDBCProperty.getURL());
+        properties.setProperty(Environment.USER, JDBCProperty.getUser());
+        properties.setProperty(Environment.PASS, JDBCProperty.getPass());
+        properties.setProperty(Environment.POOL_SIZE, "1");
+        properties.setProperty(Environment.AUTOCOMMIT, "false");
+        properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+        properties.setProperty(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        properties.setProperty(Environment.CACHE_PROVIDER_CONFIG, "org.hibernate.cache.NoCacheProvider");
+        properties.setProperty(Environment.SHOW_SQL, "true");
+
+        cfg = new Configuration();
+        cfg.setProperties(properties);
+    }
+
+    public static Configuration getConfiguration(){
+        return cfg;
+    }
 }
