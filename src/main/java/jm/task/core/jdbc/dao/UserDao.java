f836303a95de5d93818270f0d6bc20e3866ae804
lindx <linder.igor@gmail.com>
2021-01-30 23:51:42 +0300

    add UserDao and Impl

--- src/main/java/jm/task/core/jdbc/dao/UserDao.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDao.java
@@ -4,15 +4,6 @@ import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 public interface UserDao {
-    void createUsersTable();
-
-    void dropUsersTable();
-
-    void saveUser(String name, String lastName, byte age);
-
-    void removeUserById(long id);
-
-    List<User> getAllUsers();
-
-    void cleanUsersTable();
+    void add(User user);
+    List<User> listUsers();
 }
---
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoImpl.java
@@ -0,0 +1,30 @@
+package jm.task.core.jdbc.dao;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import jm.task.core.jdbc.model.User;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+}
