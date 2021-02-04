6716f438514d4c101ad488bf6ef227a6e33e7b4e
lindx <linder.igor@gmail.com>
2021-02-05 00:33:58 +0300

    logger

--- src/main/java/jm/task/core/hiber/MainApp.java
+++ src/main/java/jm/task/core/hiber/MainApp.java
@@ -1,12 +1,13 @@
 package jm.task.core.hiber;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import jm.task.core.hiber.config.AppConfig;
 import jm.task.core.hiber.model.User;
 import jm.task.core.hiber.service.UserService;
 import jm.task.core.hiber.util.Util;
-
 /**
  * MainApp
  */
@@ -14,7 +15,7 @@ public class MainApp {
 
     public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         UserService userService = context.getBean(UserService.class);
                 userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
@@ -22,10 +23,11 @@ public class MainApp {
                 userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
                 userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
 
-        userService.listUsers().stream().forEach(System.out::println);
+        userService.listUsers().stream().forEach(
 
-        Util.getLogger().info("testtestets");
+                    user -> Util.getLogger().info(user.toString())
+                );
 
-        context.close();
+        ((ConfigurableApplicationContext)context).close();
     }
 }
