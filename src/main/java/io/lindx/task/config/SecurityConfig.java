63ee554d694108bef46b60e426145d0f3886fc76
lindx <linder.igor@gmail.com>
2021-03-24 05:17:54 +0300

    add temp inMemoryAuthentication authentication

--- src/main/java/io/lindx/task/config/SecurityConfig.java
+++ src/main/java/io/lindx/task/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package io.lindx.task.config;
 
-import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -22,9 +24,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     
     http
-      . ()
+      .authorizeRequests()
         .antMatchers("/").permitAll()
         .antMatchers("/users/sign_up").permitAll()
+        .antMatchers("/users/").hasAnyRole("ROLE_ADMIN")
         .anyRequest().authenticated();
       
     http
@@ -33,5 +36,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .loginProcessingUrl("/login/process")
         .usernameParameter("email")
         .passwordParameter("password");
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    auth
+      .inMemoryAuthentication()
+
+        .withUser("admin@admin")
+        .password("admin")
+        .roles("ADMIN")
+
+      .and()
+
+        .withUser("user@user")
+        .password("user")
+        .roles("USER");
+  }
+
+  @Bean
+  public static NoOpPasswordEncoder passwordEncoder() {
+      return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
   }
 }
